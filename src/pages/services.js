f7352072c3dad3393768dcfa550e22ddfb391b03
uguroksuz <uguroksuz@outlook.com>
2019-12-02 03:09:20 +0300

    sevices page update

--- src/pages/services.js
+++ src/pages/services.js
@@ -33,43 +33,63 @@ export default class Services extends React.Component {
       <Layout>
         <SEO title="JAMStack" />
         <div className="py-5">
-          <div className="container py-5">
+          <div className="container pt-5">
             <div className="jamstack">
-              <div className="intro pb-5">
-                <BlockContent blocks={content.blockTop || []} />
-              </div>
-              <div className="letter-definition">
-                <img className="letter" src="https://d33wubrfki0l68.cloudfront.net/5beea77e9c64899ebbafbee2086fa735c8c3b1af/19e3c/img/j.svg" alt="J" />
-                <div className="definitin-content">
-                  <BlockContent blocks={content.blockJ || []} />
+              {typeof content.blockTop != 'undefined' ?
+                <div className="intro">
+                  <BlockContent blocks={content.blockTop || []} />
                 </div>
-              </div>
-              <div className="letter-definition">
-                <img className="letter" src="https://d33wubrfki0l68.cloudfront.net/db9f1475910e1dc05d452357e78bf927c9579de1/b3311/img/a.svg" alt="A" />
-                <div className="definitin-content">
-                  <BlockContent blocks={content.blockA || []} />
+                : null}
+              {typeof content.blockJ != 'undefined' ?
+                <div className="letter-definition">
+                  <img className="letter" src="https://d33wubrfki0l68.cloudfront.net/5beea77e9c64899ebbafbee2086fa735c8c3b1af/19e3c/img/j.svg" alt="J" />
+                  <div className="definitin-content">
+                    <BlockContent blocks={content.blockJ || []} />
+                  </div>
                 </div>
-              </div>
-              <div className="letter-definition">
-                <img className="letter" src="https://d33wubrfki0l68.cloudfront.net/e3922ce2101d421f658b0b11539d9d5ae95f1daa/49ce3/img/m.svg" alt="M" />
-                <div className="definitin-content">
-                  <BlockContent blocks={content.blockM || []} />
+                : null}
+              {typeof content.blockA != 'undefined' ?
+                <div className="letter-definition">
+                  <img className="letter" src="https://d33wubrfki0l68.cloudfront.net/db9f1475910e1dc05d452357e78bf927c9579de1/b3311/img/a.svg" alt="A" />
+                  <div className="definitin-content">
+                    <BlockContent blocks={content.blockA || []} />
+                  </div>
                 </div>
-              </div>
-              <section className="jam-disqualifications my-5 pt-5">
-                <BlockContent blocks={content.blockDisqualifications} />
-              </section>
-              <section id="why" className="advantages contained">
-                <BlockContent blocks={content.blockWhy || []} />
-              </section>
+                : null}
+              {typeof content.blockM != 'undefined' ?
+                <div className="letter-definition">
+                  <img className="letter" src="https://d33wubrfki0l68.cloudfront.net/e3922ce2101d421f658b0b11539d9d5ae95f1daa/49ce3/img/m.svg" alt="M" />
+                  <div className="definitin-content">
+                    <BlockContent blocks={content.blockM || []} />
+                  </div>
+                </div>
+                : null}
+              {typeof content.blockM2 != 'undefined' ?
+                <div className="letter-definition">
+                  <img className="letter" src="https://d33wubrfki0l68.cloudfront.net/e3922ce2101d421f658b0b11539d9d5ae95f1daa/49ce3/img/m.svg" alt="M" />
+                  <div className="definitin-content">
+                    <BlockContent blocks={content.blockM2 || []} />
+                  </div>
+                </div>
+                : null}
+              {typeof content.blockDisqualifications != 'undefined' ?
+
+                <section className="jam-disqualifications my-5 pt-5">
+                  <BlockContent blocks={content.blockDisqualifications} />
+                </section>
+                : null}
+              {typeof content.blockWhy != 'undefined' ?
+                <section id="why" className="advantages contained">
+                  <BlockContent blocks={content.blockWhy || []} />
+                </section>
+                : null}
             </div>
           </div>
-          <div className="container py-5">
+          <div className="container pb-5">
             <BlockContent blocks={this.state.body || []} />
           </div>
         </div>
       </Layout>
     )
   }
-
 }
