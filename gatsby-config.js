1d9ac2fac0229277adbe1bbe5685b348699b3c4a
uguroksuz <uguroksuz@outlook.com>
2019-12-03 02:17:42 +0300

    config update

--- gatsby-config.js
+++ gatsby-config.js
@@ -86,13 +86,5 @@ module.exports = {
           })
       }
     },
-    {
-      resolve: 'gatsby-plugin-robots-txt',
-      options: {
-        host: 'https://www.nordensoft.ch',
-        sitemap: 'https://www.nordensoft.ch/sitemap.xml',
-        policy: [{ userAgent: '*', allow: '/' }]
-      }
-    }
   ],
 }
